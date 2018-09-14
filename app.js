009070274180b9ecbf14b55b5787291277ec47ab
benjaminet <benjamintimmermann@gmail.com>
2018-09-14 14:30:01 +0200

    added website

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('WELCOME!!!'); // popup alert
+console.log('Hello World'); //String
+
+/*
+function jep(rette, snor) {
+    if (rette == 10) return a+1;
+    else return rette + snor;
+}
+console.log(jep(10, 5));
+print.toString(jep);
+
+let anObject = {left: 1, right: 2};
+console.log(anObject.left);
+*/
